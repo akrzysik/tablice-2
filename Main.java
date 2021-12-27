fa646af606338c00e9ae3429e1436c5afbdb4955
akrzysik <95127620+akrzysik@users.noreply.github.com>
2021-12-27 18:51:01 +0000

    srednia

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
+   public static int losuj(int max) {
+      Random rand = new Random();
+      return rand.nextInt(max);
+   }
 
    public static int suma(int[] tablica, int rozmiar) {
       int s = 0;
@@ -16,6 +22,10 @@ public class Main {
             min = tablica[i];
       return min;
    }
+
+   public static int srednia(int[] tablica, int rozmiar) {
+      return suma(tablica, rozmiar) / rozmiar;
+   }
    
    public static void wczytaj_tablice(int[] tablica, int rozmiar) throws IOException {
       BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -25,13 +35,26 @@ public class Main {
       }
    }
    
+   public static void losuj_tablice(int[] tablica, int rozmiar, int max) {
+    for (int i = 0; i < rozmiar; i++) {
+      tablica[i] = losuj(max);
+    }
+  }
+
    public static void wypisz_tablice(int[] tablica, int rozmiar) {
       for ( int i = 0; i < rozmiar; i++ )
          System.out.println("tablica["+i+"] = "+tablica[i]);
    }
 
    public static void main(String[] args) {
-      
+      int rozmiar = 6;
+      int[] t = new int[rozmiar];
+      losuj_tablice(t, rozmiar, 200);
+      wypisz_tablice(t, rozmiar);
+
+      System.out.println("suma: " + suma(t, rozmiar));
+      System.out.println("minimum: " + minimum(t, rozmiar));
+      System.out.println("srednia: " + srednia(t, rozmiar));
    }
 }
 
